8daceee993de17ba58762b7817ac9ef2de039ddd
JyothiRadithyaJalla <jyothijalla49@gmail.com>
2026-03-05 20:41:10 +0530

    fix backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import men_banner from "./Components/Assets/banner_mens.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
 import LoginSignup from "./Pages/LoginSignup";
 
-export const backend_url = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+export const backend_url = "https://ecommerce-mern-gtek.onrender.com";
 // console.log("Frontend Backend URL:", backend_url);
 export const currency = '₹';
 
