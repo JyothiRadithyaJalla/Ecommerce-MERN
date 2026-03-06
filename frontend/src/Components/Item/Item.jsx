1b7271864c9a4472467e2619ec22ce142d38e2fc
JyothiRadithyaJalla <jyothijalla49@gmail.com>
2026-03-06 19:42:56 +0530

    fiximage loading

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -13,7 +13,7 @@ const Item = (props) => {
         <img
   
           onClick={() => window.scrollTo(0,0)}
-              src={props.image}
+              src={`${backend_url}/images/${props.image}`}
           alt="products"
         />
 
